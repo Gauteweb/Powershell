831e2991cc469f062923231c28b250565c53c2bd
Gaute Holmin <gaute@topdogs.no>
2021-12-07 23:53:12 +0100

    Add files via upload
    Do you have problems starting the Good Old Games Galaxy application because there are lock files and/or processes started by other users on your computer? This script will kill all the processes, delete the lock-files and restart the GOG Galaxy application for you :-)

---
+++ Kill-Start-GOGGalaxy.ps1
@@ -0,0 +1,20 @@
+# To be able to run Powershell scripts, make sure you have unrestricted access.
+# Run get-executionpolicy to check
+# To change policy, run Powershell as administrator and type set-executionpolicy Unrestricted
+
+# This script has to be run by an administrator
+# Comment out this if you have disabled UAC
+Start-Process Powershell -Verb runAs
+
+# Kill all the GOG services
+taskkill /IM "GalaxyCommunication.exe" /F
+taskkill /IM "GalaxyClient Helper.exe" /F
+taskkill /IM "GOG Galaxy Notifications Renderer.exe" /F
+taskkill /IM "GalaxyClient.exe" /F
+
+# Delete the lock-files (you may have to change the installation path)
+Remove-Item "C:\ProgramData\GOG.com\Galaxy\lock-files\GalaxyClient.exe-galaxy-client.lock"
+Remove-Item "C:\ProgramData\GOG.com\Galaxy\lock-files\GOG Galaxy Notifications Renderer.exe.lock"
+
+# Start GOG Galaxy Client
+& "C:\Program Files (x86)\GOG Galaxy\GalaxyClient.exe"
